b2ec65bddd2b17ab1d69cd3bed6c1592b6a4ede7
thatcatfromspace <encyclopedia748@gmail.com>
2023-08-30 15:40:11 +0530

    feat: dashboard

---
+++ src/Dashboard.jsx
@@ -0,0 +1,9 @@
+export const Dashboard = () => {
+  return (
+    <div className="back bg-gradient-to-tr from-fuchsia-400 to-indigo-500">
+      <div className="front-window min-h-[95%] min-w-[95%] rounded-lg bg-slate-50">
+        
+      </div>
+    </div>
+  )
+}
