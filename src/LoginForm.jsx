83bd9e4899708d0726c0532388edab3e6316bad7
thatcatfromspace <encyclopedia748@gmail.com>
2023-08-31 07:23:22 +0530

    not my proudest commit

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import * as yup from 'yup';
 import { Link } from "react-router-dom";
 import { yupResolver } from "@hookform/resolvers/yup";
+// import { useState } from "react";
 
 import { Rule } from "./Rule"
 
@@ -14,6 +15,48 @@ const schema = yup.object().shape({
   password: yup.string().min(4).max(20).required("Password is a required field."),
 });
 
+// const [toggleValue, setToggleValue] = useState(false);
+
+// const set = () => {
+//   setToggleValue(!toggleValue);
+// };
+
+let toggle = document.getElementById("password-toggle");
+let passwordInput = document.getElementById("password-input");
+
+const showHidePassword = () => {
+  if (passwordInput.type == "password") {
+    passwordInput.setAttribute('type', 'text');
+  }
+  else if (passwordInput.type == "text"){
+    passwordInput.setAttribute('type', 'password');
+  }
+};
+
+toggle.addEventListener('click', showHidePassword);
+
+// const PasswordField = () => {
+//   if (toggleValue){
+//     return ( 
+//       <div>
+//         <input type="password" id="password-input" className="text-sm w-64 px-2 h-10 bg-gray2 rounded-md outline-none focus:outline-primary focus:border-0 me-2 absolute" required {...register("password")}></input>
+//         <Link id="password-toggle">
+//           <svg className="relative top-2 left-56" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 3l18 18M10.5 10.677a2 2 0 002.823 2.823" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7.362 7.561C5.68 8.74 4.279 10.42 3 12c1.889 2.991 5.282 6 9 6 1.55 0 3.043-.523 4.395-1.35M12 6c4.008 0 6.701 3.158 9 6a15.66 15.66 0 01-1.078 1.5" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+//         </Link>
+//       </div>
+//     )
+//   }
+//   else { 
+//     return (
+//     <div>
+//       <input type="password" id="password-input" className="text-sm w-64 px-2 h-10 bg-gray2 rounded-md outline-none focus:outline-primary focus:border-0 me-2 absolute" required {...register("password")}></input>
+//       <Link id="password-toggle">
+//         <svg className="relative top-2 left-56" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 14a2 2 0 100-4 2 2 0 000 4z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M21 12c-1.889 2.991-5.282 6-9 6s-7.111-3.009-9-6c2.299-2.842 4.992-6 9-6s6.701 3.158 9 6z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+//       </Link>
+//     </div>
+//   )}
+// };
+
 export const LoginForm = () => {
   const { register, handleSubmit } = useForm({
     resolver: yupResolver(schema),
@@ -21,7 +64,7 @@ export const LoginForm = () => {
 
   return (
     <div onSubmit={handleSubmit(onSubmit)} className="login-back bg-slate-700 w-full h-screen justify-end min-h-64 flex">
-      <form className="login-form min-h-[50vh] h-[50vh] py-3 px-3 w-64 my-52 me-20">
+      <form className="login-form min-h-[50vh] h-[50vh] py-3 px-3 w-72 my-52 me-20">
         <label className="flex justify-center pt-2 font-bold text-2xl mb-6" htmlFor="continue-with"> Continue with </label>
         <div className="login-icons flex justify-evenly mx-14 mb-5">
           <Link to="https://github.com" target="_blank">
@@ -36,17 +79,22 @@ export const LoginForm = () => {
           </Link>
         </div>
         <Rule />
-        <div className="flex flex-col px-2 pt-5 pb-4">
+        <div className="flex flex-col px-2 pt-5 pb-4 justify-center">
           <fieldset>
             <legend><p className="text-lg mb-1"> Email </p></legend>
           </fieldset>
-        <input type="email" className="text-sm px-2 h-10 bg-gray2 rounded-md outline-none focus:outline-primary focus:border-0 me-2" autoFocus={true} required {...register("email")} ></input>
+        <input type="email" className="text-md w-64 px-2 h-10 bg-gray2 rounded-md outline-none focus:outline-primary focus:border-0 me-2" autoFocus required {...register("email")} ></input>
         </div>
         <div className="flex flex-col px-2 pt-2 pb-4">
           <fieldset>
             <legend><p className="text-lg mb-1"> Password </p></legend>
           </fieldset>
-          <input type="password" className="text-sm px-2 h-10 bg-gray2 rounded-md outline-none focus:outline-primary focus:border-0 me-2" required {...register("password")}></input>
+          <div>
+            <input type="password" id="password-input" className="text-sm w-64 px-2 h-10 bg-gray2 rounded-md outline-none focus:outline-primary focus:border-0 me-2 absolute" required {...register("password")}></input>
+            <Link id="password-toggle">
+              <svg className="relative top-2 left-56" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M3 3l18 18M10.5 10.677a2 2 0 002.823 2.823" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7.362 7.561C5.68 8.74 4.279 10.42 3 12c1.889 2.991 5.282 6 9 6 1.55 0 3.043-.523 4.395-1.35M12 6c4.008 0 6.701 3.158 9 6a15.66 15.66 0 01-1.078 1.5" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+            </Link>
+          </div>
         </div>
         <div className="flex justify-center py-5">
           <button type="submit" class="text-white bg-gradient-to-br from-gray0 to-gray1 hover:bg-gradient-to-bl focus:ring-4 outline-none focus:outline-none font-medium rounded-full text-md px-5 py-2.5 text-center mr-2 mb-2">Login</button>
--- src/Rule.jsx
+++ src/Rule.jsx
@@ -1,13 +1,13 @@
 export const Rule = () => (
   <div className="rules flex mx-2 justify-center">
     <div className="align-middle mt-2">
-      <hr className=" border border-gray-300 w-24" />
+      <hr className=" border border-gray-300 w-28" />
     </div>
     <div className="mx-2">
       <span> OR </span>
     </div>
     <div className="align-middle pt-2">
-      <hr className=" border border-gray-300 w-24" />
+      <hr className=" border border-gray-300 w-28" />
     </div>
   </div>
 );
