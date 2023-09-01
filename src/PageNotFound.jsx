db4d9c2c90261940ebcbce2c46537110f6ea8fd1
Mithun@2303 <mithunkarthick1610@gmail.com>
2023-09-01 19:14:33 +0530

    404 page touch up

--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -2,8 +2,8 @@ import Lottie from "lottie-react";
 import notFound from "./assets/notFound.json";
 
 export const NotFound = () => (
-  <div className="flex flex-col self-center items-center justify-center w-1/4 h-1/4">
-    <Lottie animationData={notFound} loop={true}/>
-    <p className="text-lg text-center"> {"The page you requested could not be found :("} </p>
+  <div className="relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%]">
+    <Lottie animationData={notFound} loop={true} className="h-[30vh]"/>
+    <p className="text-lg text-center "> {"The page you requested could not be found :("} </p>
   </div>
 );
