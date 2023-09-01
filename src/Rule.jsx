9d11b1adfe7c8731eaa47bb5a13a7a485b7d5d90
thatcatfromspace <encyclopedia748@gmail.com>
2023-09-01 18:17:19 +0530

    improved ui/ux

--- src/Rule.jsx
+++ src/Rule.jsx
@@ -1,7 +1,7 @@
 export const Rule = () => (
   <div className="flex w-full justify-between items-center">
-    <div className="w-full bg-gray-500 h-[1px]" />
+    <div className="w-full bg-gray-500 h-[1px] mx-2" />
     <div>{"OR"}</div>
-    <div className="w-full  bg-gray-500 h-[1px]" />
+    <div className="w-full bg-gray-500 h-[1px] mx-2" />
   </div>
 );
