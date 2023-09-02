a8f7b70fc84592296bed0feed720ce7921dc1bfe
thatcatfromspace <encyclopedia748@gmail.com>
2023-09-02 06:28:28 +0530

    minor tweaks

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -59,22 +59,26 @@ export const LoginForm = () => {
         </div>
         <div className="mt-8">
           <input type={toggleValue? "text": "password"} className="text-md bg-gray2 w-[276px] rounded-2xl lg:w-[274px] h-12 px-4 outline-none placeholder:text-gray-500 absolute" placeholder="Password" {...register("password")}></input>
-          <Link onClick={toggleSetValue}>
+          <span className="hover:cursor-pointer" onClick={toggleSetValue}>
             <RenderEye />
-          </Link>
+          </span>
         </div>
         <div className="text-right mt-14">
           <Link className="text-blue-500 hover:underline visited:text-blue-500"> {"Forgot password?"} </Link>
         </div>
         <div className="my-8 flex justify-center">
-          <button className="bg-tertiary text-gray3 shadow-lg shadow-gray1 px-8 py-2.5 rounded-full hover:opacity-80 transition-opacity hover:transition-opacity"> {"Login"} </button>
+          <button className="bg-tertiary font-bold text-gray3 shadow-lg shadow-gray1 px-8 py-2.5 rounded-full hover:opacity-80 transition-opacity hover:transition-opacity"> {"Login"} </button>
         </div>
         <Rule />
         <div className="flex justify-evenly mt-3">
           <button className="shadow-lg rounded-full px-3 py-3 bg-gray2">
-            <svg class="w-7 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-              <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
-            </svg>
+          <svg className="h-7 w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
+            <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
+            <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
+            <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
+            <path d="M1 1h22v22H1z" fill="none"/>
+          </svg>
           </button>
           <button className="shadow-lg rounded-full px-3 py-3 bg-gray2">
             <svg class="w-7 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
