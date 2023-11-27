bc597bca1a162619ab0fb728e16a269dfa487d45
thatcatfromspace <encyclopedia748@gmail.com>
2023-11-27 11:12:36 +0530

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { LoginForm } from "./LoginForm";
 import { SignupForm } from "./SignupForm";
-import { NotFound } from "./PageNotFound"
+import { NotFound } from "./PageNotFound";
+import { Dashboard } from "./Dashboard";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,9 +1,7 @@
 export const Dashboard = () => {
   return (
-    <div className="back bg-gradient-to-tr from-fuchsia-400 to-indigo-500">
-      <div className="front-window min-h-[95%] min-w-[95%] rounded-lg bg-slate-50">
-        
-      </div>
+    <div className="back bg-gradient-to-tr from-fuchsia-400 to-indigo-500 h-auto">
+      <div className="front "></div>
     </div>
   )
 }
