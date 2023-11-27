da3ccdac023d78fde288e5341d0dc73520edf321
thatcatfromspace <encyclopedia748@gmail.com>
2023-11-27 13:50:31 +0530

    dashboard navbar

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "127.0.0.1",
+    host: "192.168.1.16",
     port: 8000,
   },
 });
