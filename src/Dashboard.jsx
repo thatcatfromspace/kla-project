dce63ff013aaf96b80bf5b54199cd87cef8139e3
thatcatfromspace <encyclopedia748@gmail.com>
2023-11-30 21:28:26 +0530

    added assets

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -10,6 +10,7 @@ export const Dashboard = () => {
   const [activity, setActivity] = useState([]);
   const [radioOptions, setRadioOptions] = useState([]);
   const [tempFlag, setTempFlag] = useState(false);
+
   const changeActiveElement = (e, itemId) => {
     e.preventDefault();
     setActiveElement(itemId);
@@ -264,7 +265,7 @@ export const Dashboard = () => {
       </nav>
       <div className="cards flex w-[40vw] mt-[3vh] ml-[3vw] ">
         <button onClick={(e) => previousCard(e)} className="w-[10%]">
-          PREV
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg> {/* make stroke color gray if going to previous is disabled! */}
         </button>
         <ul className="flex flex-col">
           {card.questions &&
@@ -300,7 +301,7 @@ export const Dashboard = () => {
             ))}
         </ul>
         <button onClick={(e) => nextCard(e)} className="w-[10%]">
-          NEXT
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
         </button>
       </div>
     </main>
